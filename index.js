b7d9baa34d75f08b2d8a6152dac85eb1f69a6218
Thanthiti <plam1234palm@gmail.com>
2024-02-23 15:10:11 +0700

    sd

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ var bodyParser = require('body-parser')
 const path = require("path")
 
 
-
+console.log("sd")
 
 const base_url = "http://node57328-nodepem.proen.app.ruk-com.cloud"
-    //const base_url = "http://node58886-test-node.proen.app.ruk-com.cloud"
-    //const base_url = "http://localhost:3000"
+
+//const base_url = "http://node58886-test-node.proen.app.ruk-com.cloud"
+//const base_url = "http://localhost:3000"
 app.set("views", path.join(__dirname, "/public/views"))
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
